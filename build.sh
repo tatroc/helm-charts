a8ce981c4cb7c9cc536bfba166dab583e53d1c89
Chris Tatro <christopher.tatro@kaplan.edu>
2022-10-25 10:30:53 -0500

    test

--- build.sh
+++ build.sh
@@ -9,3 +9,9 @@ helm package charts/helm-test-chart --version $version
 
 helm repo index --url https://tatroc.github.io/helm-charts/ .
 cat index.yaml
+
+# helm repo add tatro-github https://tatroc.github.io/helm-charts
+# helm search repo tatro-github
+# helm install helm-test-chart --set docker_hub_secret=$DOCKER_HUB_SECRET tatro-github/helm-test-chart -n test
+
+# helm delete helm-test-chart -n test
