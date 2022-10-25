5197d878563201ec0bc03fb8d4655ae1c05261b7
Chris Tatro <christopher.tatro@kaplan.edu>
2022-10-25 08:41:47 -0500

    test

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export version=1.0.1
+export chart_name=helm-test-chart
+
+
+#cd helm-test-chart 
+
+helm package charts/helm-test-chart --version $version
+
+
+helm repo index --url https://github.com/tatroc/helm-charts/ . 
